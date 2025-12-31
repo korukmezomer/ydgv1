81dca22c47f5261fc7856f9b6679026fef75e897
omerkorukmez <korukmezomer1@gmail.com>
2026-01-01 00:57:31 +0300

    Add missing JavascriptExecutor import to Case14

--- backend/src/test/java/com/example/backend/selenium/Case14_WriterEditStoryTest.java
+++ backend/src/test/java/com/example/backend/selenium/Case14_WriterEditStoryTest.java
@@ -3,6 +3,7 @@ package com.example.backend.selenium;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
